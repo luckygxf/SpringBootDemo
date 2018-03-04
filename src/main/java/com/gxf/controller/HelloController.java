f1836102a3bc74867b23a70356234aebadabc7fa
luckygxf <403261934@qq.com>
2018-03-05 00:50:43 +0800

    first commit

---
+++ src/main/java/com/gxf/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.gxf.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by GuanXF on 2018/3/5.
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
