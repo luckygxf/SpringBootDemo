511f8a67de28dfc297189d8acc5111eb71b31a22
luckygxf <403261934@qq.com>
2018-03-06 20:20:17 +0800

    @PropertySource(value = {"classpath:gxf.properties"})指定配置文件

--- src/main/java/com/gxf/Application.java
+++ src/main/java/com/gxf/Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 
 import java.util.Arrays;
 
@@ -15,6 +16,7 @@ import java.util.Arrays;
  * Created by GuanXF on 2018/3/5.
  */
 @SpringBootApplication
+@PropertySource(value = {"classpath:gxf.properties"})
 public class Application {
     @Autowired
     private DIEnvironment diEnvironment;
