14f9f325277677288c4506541be637392cef53a6
luckygxf <403261934@qq.com>
2018-04-10 12:08:19 +0800

    《深入剖析tomcat》代码 | chapter6 | log demo

--- src/main/java/com/gxf/how_tomcat/chapter7/TestException.java
+++ src/main/java/com/gxf/how_tomcat/chapter7/TestException.java
@@ -5,6 +5,20 @@ package com.gxf.how_tomcat.chapter7;
  */
 public class TestException {
     public static void main(String[] args) {
+        try{
+            test();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            StackTraceElement[] stackTraceElements = e.getStackTrace();
+            for(StackTraceElement stackTraceElement : stackTraceElements){
+                System.out.println(stackTraceElement.getClassName() + ", " + stackTraceElement.getMethodName() + ", " + stackTraceElement.getFileName()
+                                + ", " + stackTraceElement.getLineNumber());
+            }
+            e.printStackTrace();
+        }
+    }
+
+    private static void test(){
         int a = 1 / 0;
     }
 }
