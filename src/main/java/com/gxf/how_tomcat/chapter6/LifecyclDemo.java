f7a256eccf9f8bf5e8faa5f7b585452dc96861f3
luckygxf <403261934@qq.com>
2018-04-10 09:55:26 +0800

    《深入剖析tomcat》代码 | chapter6 | lifecycle

---
+++ src/main/java/com/gxf/how_tomcat/chapter6/LifecyclDemo.java
@@ -0,0 +1,56 @@
+package com.gxf.how_tomcat.chapter6;
+
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.LifecycleListener;
+import org.apache.catalina.LifecycleState;
+
+/**
+ * Created by 58 on 2018/4/10.
+ */
+public class LifecyclDemo implements Lifecycle {
+    @Override
+    public void addLifecycleListener(LifecycleListener listener) {
+
+    }
+
+    @Override
+    public LifecycleListener[] findLifecycleListeners() {
+        return new LifecycleListener[0];
+    }
+
+    @Override
+    public void removeLifecycleListener(LifecycleListener listener) {
+
+    }
+
+    @Override
+    public void init() throws LifecycleException {
+
+    }
+
+    @Override
+    public void start() throws LifecycleException {
+
+    }
+
+    @Override
+    public void stop() throws LifecycleException {
+
+    }
+
+    @Override
+    public void destroy() throws LifecycleException {
+
+    }
+
+    @Override
+    public LifecycleState getState() {
+        return null;
+    }
+
+    @Override
+    public String getStateName() {
+        return null;
+    }
+}
