5889d26fe95646fee8b9a11464c9d4b8ebd81c15
luckygxf <403261934@qq.com>
2018-04-22 09:32:51 +0800

    添加拦截器demo

--- src/main/java/com/gxf/controller/HelloController.java
+++ src/main/java/com/gxf/controller/HelloController.java
@@ -2,11 +2,8 @@ package com.gxf.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ua_parser.Client;
-import ua_parser.Parser;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 
 /**
  * Created by GuanXF on 2018/3/5.
@@ -18,21 +15,6 @@ public class HelloController {
     public String index(HttpServletRequest request) {
         String userAgent = request.getHeader("User-Agent");
         System.out.println("userAgent: " + userAgent);
-        try {
-            Parser parser = new Parser();
-            Client c = parser.parse(userAgent);
-            System.out.println(c.userAgent.family); // => "Mobile Safari"
-            System.out.println(c.userAgent.major);  // => "5"
-            System.out.println(c.userAgent.minor);  // => "1"
-
-            System.out.println(c.os.family);        // => "iOS"
-            System.out.println(c.os.major);         // => "5"
-            System.out.println(c.os.minor);         // => "1"
-
-            System.out.println(c.device.family);    // => "iPhone"
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
         return "Greetings from Spring Boot!";
     }
---
+++ src/main/java/com/gxf/interceptors/MyWebAppConfigure.java
@@ -0,0 +1,16 @@
+package com.gxf.interceptors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+public class MyWebAppConfigure extends WebMvcConfigurerAdapter {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new MyInterceptor1()).addPathPatterns("/**");
+        registry.addInterceptor(new MyInterceptor2()).addPathPatterns("/**");
+        super.addInterceptors(registry);
+    }
+}
