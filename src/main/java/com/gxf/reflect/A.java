64ad2092f1d21427389d4d42c209a826993e9891
luckygxf <403261934@qq.com>
2018-04-30 01:49:37 +0800

    添加reflect

--- src/main/java/com/gxf/reflect/A.java
+++ src/main/java/com/gxf/reflect/A.java
@@ -4,6 +4,25 @@ package com.gxf.reflect;
  * Created by 58 on 2018/4/28.
  */
 public class A {
+    private String I;
+    private String name;
+
+    public String getI() {
+        return I;
+    }
+
+    public void setI(String i) {
+        I = i;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public void foo(String name){
         System.out.println("hello , " + name);
     }
--- src/main/java/com/gxf/reflect/TestClassLoad.java
+++ src/main/java/com/gxf/reflect/TestClassLoad.java
@@ -1,6 +1,7 @@
 package com.gxf.reflect;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 /**
@@ -9,9 +10,61 @@ import java.lang.reflect.Method;
 public class TestClassLoad {
 
     public static void main(String[] args) throws Exception {
-        testNewInstance();
+        doGet();
+        doReflectGet();
     }
 
+    public static void doGet() throws Exception {
+        long start = System.currentTimeMillis();
+        A a = new A();
+        for (int i = 0; i < 100000000; i++) {
+            a.getI();
+        }
+        System.out.println(System.currentTimeMillis() - start);
+    }
+    public static void doReflectGet() throws Exception {
+        long start = System.currentTimeMillis();
+        A a = new A();
+        Class<A> cls = A.class;
+        Field field = cls.getDeclaredField("I");
+        field.setAccessible(true);
+        for (int i = 0; i < 100000000; i++) {
+            field.get(a);
+        }
+        System.out.println(System.currentTimeMillis() - start);
+    }
+
+
+    private static void testInstance() throws Exception {
+        String classPackage = "com.gxf.reflect.Person";
+        Class clazz = Class.forName(classPackage);
+        Class[] params = {String.class, Integer.class};
+        Object[] values = {"guanxiangfei", 28};
+        Constructor constructor = clazz.getConstructor(params);
+        Person guanxiangfei = (Person) constructor.newInstance(values);
+        System.out.println(guanxiangfei);
+
+    }
+
+
+    private static void testView(){
+        int a = 0;
+        Integer b = 0;
+        if(b == a){
+            System.out.println("a");
+        }
+        else{
+            System.out.println("b");
+        }
+    }
+
+
+    private static void testReflect() throws Exception{
+        Class a = A.class;
+        Constructor constructor = a.getConstructor(a);
+        A instance = (A) constructor.newInstance();
+        System.out.println(instance);
+    }
 
     public static void testMethod() throws Exception{
         Class<?> clazz = Class.forName("com.gxf.reflect.A");
